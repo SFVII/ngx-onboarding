0e0cfeaa5d1f2f98e5eaf6d57b2563c70bd2512f
diary narindra <diarynar@gmail.com>
2021-11-29 20:25:21 +0300

    fix: url

--- lib/onboarding.component.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnInit } from '@angular/core';
-import { OnboardingService } from './onboarding.service';
-import { Router } from '@angular/router';
-export declare class OnboardingComponent implements OnInit {
-    private service;
-    private router;
-    constructor(service: OnboardingService, router: Router);
-    ngOnInit(): void;
-}
--- lib/onboarding.module.d.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { OnboardingInterface } from '../interface/OnboardingInterface';
-import { Routes } from '@angular/router';
-export declare const onBoardingRoutes: Routes;
-export declare class OnboardingModule {
-    constructor(parentModule?: OnboardingModule);
-    static forRoot(config: OnboardingInterface): ModuleWithProviders<OnboardingModule>;
-}
